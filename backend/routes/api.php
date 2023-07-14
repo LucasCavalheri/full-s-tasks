c93b6f24b758d1088e1563d294baf046d06ea0fa
LucasCavalheri <cav.catecumenal@gmail.com>
2023-07-14 15:54:02 -0300

    fix: pequenas correções

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,24 +18,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::prefix('/auth')->group(function () {
     Route::post('/login', LoginController::class);
     Route::post('/register', RegisterController::class);
     Route::post('/logout', LogoutController::class)->middleware(['auth:sanctum']);
 });
 
-// Route::middleware('auth:sanctum')->prefix('/tasks')->group(function () {
-//     Route::post('/', [TaskController::class, 'store']);
-//     Route::get('/', [TaskController::class, 'index']);
-//     Route::get('/{id}', [TaskController::class, 'show']);
-//     Route::patch('/{id}', [TaskController::class, 'update']);
-//     Route::delete('/{id}', [TaskController::class, 'destroy']);
-// });
-
-Route::prefix('/tasks')->group(function () {
+Route::middleware('auth:sanctum')->prefix('/tasks')->group(function () {
     Route::post('/', [TaskController::class, 'store']);
     Route::get('/', [TaskController::class, 'index']);
     Route::get('/{id}', [TaskController::class, 'show']);
     Route::patch('/{id}', [TaskController::class, 'update']);
     Route::delete('/{id}', [TaskController::class, 'destroy']);
 });
+
+// Route::prefix('/tasks')->group(function () {
+//     Route::post('/', [TaskController::class, 'store']);
+//     Route::get('/', [TaskController::class, 'index']);
+//     Route::get('/{id}', [TaskController::class, 'show']);
+//     Route::patch('/{id}', [TaskController::class, 'update']);
+//     Route::delete('/{id}', [TaskController::class, 'destroy']);
+// });
